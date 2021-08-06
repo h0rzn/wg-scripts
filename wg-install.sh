eea145acf88af082145b0efadb0b05d5e5f15dc5
h0rzn <hrzn@protonmail.com>
2021-08-06 08:08:28 +0200

    added debian install function

--- wg-install.sh
+++ wg-install.sh
@@ -3,9 +3,12 @@
 # check for distro
 
 function install_on_debian() {
-    sudo sh -c "echo 'deb http://deb.debian.org/debian buster-backports main' >> /etc/apt/sources.list.d/backports.list"
-    sudo sh -c "printf 'Package: *\nPin: release a=buster-backports\nPin-Priority: 90\n' >> /etc/apt/preferences.d/limit-backports"
+    sh -c "echo 'deb http://deb.debian.org/debian buster-backports main' >> /etc/apt/sources.list.d/backports.list"
+    sh -c "printf 'Package: *\nPin: release a=buster-backports\nPin-Priority: 90\n' >> /etc/apt/preferences.d/limit-backports"
+    apt update
+    apt install wireguard --assume-yes
 
+    apt install wireguard-dkms wireguard-tools linux-headers-$(uname -r) --assume-yes
     
 }
 
